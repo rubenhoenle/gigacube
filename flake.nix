d0984ec98e9888bc32ff9defc5d0e0de265a6903
Ruben Hoenle <git@hoenle.xyz>
2024-03-16 01:50:26 +0100

    feat(nix): add nix formatter

--- flake.nix
+++ flake.nix
@@ -1,31 +1,43 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: 
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-  in
-  {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        adafruit-ampy
-      ];
-      env = {
-        AMPY_PORT = "/dev/ttyACM0";
-      };
-      shellHook = ''
-        alias help="echo \"COMMANDS: deploy, reset, run\""
-        alias deploy="ampy put lib && ampy put webserver.py"
-        alias reset="ampy reset"
-        alias run="ampy run main.py"
-        alias npmCi="ampy put lib && ampy put webserver.py"
-        alias cleanEclipse="ampy reset"
-        alias buildCommand="ampy run main.py"
-      '';
-    }; 
-
   };
+
+  outputs = { self, nixpkgs, treefmt-nix }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+    in
+    {
+      formatter.${system} = treefmtEval.config.build.wrapper;
+      checks.${system}.formatter = treefmtEval.config.build.check self;
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          adafruit-ampy
+        ];
+        env = {
+          AMPY_PORT = "/dev/ttyACM0";
+        };
+        shellHook = ''
+          alias help="echo \"COMMANDS: deploy, reset, run\""
+          alias deploy="ampy put lib && ampy put webserver.py"
+          alias reset="ampy reset"
+          alias run="ampy run main.py"
+          alias npmCi="ampy put lib && ampy put webserver.py"
+          alias cleanEclipse="ampy reset"
+          alias buildCommand="ampy run main.py"
+        '';
+      };
+
+    };
 }
