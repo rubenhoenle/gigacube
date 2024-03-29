6ef26373bd8431ab1cba993189d5ff72100e6d77
Tim Lanzinger <tim.lanzinger@gmx.de>
2024-03-29 18:45:11 +0100

    init VisualCube

--- faradaycage/neopixelLibary/neopixel/neopixel.py
+++ faradaycage/neopixelLibary/neopixel/neopixel.py
@@ -2,6 +2,26 @@
 
 class NeoPixel:
 
-    def __init__(self):
+    def __init__(self, pin, length):
 
+        self.pin = pin
+        self.length = length
+
+        self.data = [None for _ in range(self.length)]
         print("Hello from emulated class")
+    
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __setitem__(self, index, color: tuple):
+        # validate color
+        return self.data[index] = color
+
+    def write(self):
+        self.show()
+
+    def show(self):
+        pass
+
+    def fill(sefl, color: type):
+        self.data = [color for _ in range(self.length)]
