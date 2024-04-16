2cc606070881beac2e625d4550e5cfde0ef77aaa
Tim Lanzinger <tim.lanzinger@gmx.de>
2024-04-16 17:33:59 +0200

    init snake-io

--- lib/nunchuck.py
+++ lib/nunchuck.py
@@ -22,7 +22,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import os
-virtuell = os.getenv("VIRTUELL") == "true"
+virtuell = False
 
 print("virtuell: ", virtuell)
 
