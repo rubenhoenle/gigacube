7b419e782d7e3ed70c45fec1fab05cf1368e1f49
Tim Lanzinger <tim.lanzinger@gmx.de>
2024-04-30 21:02:29 +0200

    better colors

--- lib/fsck.py
+++ lib/fsck.py
@@ -33,29 +33,47 @@ fsck_banner = [
 
 class IdleScreen:
 
+    colors = [
+                [255, 0, 0],
+                [0, 255, 0],
+                [0, 0, 255],
+                [255, 255, 0],
+                [255, 255, 0],
+                [0, 255, 255],
+            ]
+
     def __init__(self, side: Side, displaycontroller: DisplayController):
         self.side = side
         self.displaycontroller = displaycontroller
 
         self.cells = []
 
-        self.color = (255, 255, 0)
         for y, row in enumerate(fsck_banner):
             for x, pixel_value in enumerate(row):
                 if pixel_value == 1:
                     c = CellPos(side, x, MATRIX_SIZE - y - 1)
-                    self.cells.append(c)
+                    self.cells.append([c, 0]) 
                 
 
     def update(self):
         self.displaycontroller.clearMatrix()
 
-        for c in self.cells:
+        for index, c_data in enumerate(self.cells):
+
+            c, color_index = c_data
+
             try:
                 c.move(Direction.RIGHT)
             except:
                 c.side = self.side
                 c.x = 0
-            self.displaycontroller.writePixel(c, self.color)
+                
+                new_index = color_index +1
+                if new_index >= len(self.colors):
+                    new_index = 0
+
+                self.cells[index][1] = new_index
+
+            self.displaycontroller.writePixel(c, self.colors[color_index])
         self.displaycontroller.updateMatrix()
             
